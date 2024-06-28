ada93275bcf237c1a5a27431d87dbd8920a8b0d8
Matinggff <119169794+Matinggff@users.noreply.github.com>
2024-06-28 15:26:42 +0430

    update number guessing game

--- Guess the number/js/app.js
+++ Guess the number/js/app.js
@@ -1,5 +1,6 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 
+const heartsContainer = document.getElementById("hearts-container");
 let userForm = document.querySelector(".userForm");
 let textBox = document.querySelector("#text");
 let hearts = document.querySelector(".hearts");
@@ -7,8 +8,24 @@ let gameover = document.querySelector(".body");
 
 let heart = 10;
 let attempts = 0;
+let win = false;
 
-hearts.innerText = "Hearts: " + heart;
+
+function upHearts() {
+    heartsContainer.innerHTML = '';
+    for (let i = 0; i < heart; i++) {
+        const heartItem = document.createElement("div");
+        heartItem.className = "item";
+
+        const heartImg = document.createElement("img");
+        heartImg.src = "img/heart.png";
+        heartItem.appendChild(heartImg);
+        heartsContainer.appendChild(heartItem);
+    
+    }
+}
+upHearts()
+
 
 userForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,6 +34,9 @@ userForm.addEventListener("submit", (e) => {
     if (heart === 0) {
         textBox.innerText = "GAME OVER!!";
         gameover.classList.replace("body", "gameover");
+
+        let buttonContainer = document.getElementById("buttonContainer");
+        buttonContainer.innerHTML = '';
         
         let newButton = document.createElement("button");
         newButton.classList.add("newButton");
@@ -26,35 +46,46 @@ userForm.addEventListener("submit", (e) => {
             location.reload();
         });
         
-        let buttonContainer = document.getElementById("buttonContainer");
+        
         buttonContainer.appendChild(newButton);
     } else if (guess < 1 || guess > 100) {
         textBox.innerText = "Please Enter a valid Number";
     } else {
-        attempts++;
+        if (win == false) {
+            attempts++;            
+        }
 
         if (randomNumber > guess) {
             textBox.innerText = "TOO LOW. TRY AGAIN!!";
             heart--;
-            hearts.innerText = "Hearts: " + heart;
+            upHearts()
         } else if (randomNumber < guess) {
             textBox.innerText = "TOO HIGH. TRY AGAIN!!";
             heart--;
-            hearts.innerText = "Hearts: " + heart;
+            upHearts()
         } else {
+            win=true;
             textBox.innerText = "YOU WIN! The number was " + randomNumber + ". It took you " + attempts + " attempts.";
             heart = 10;
+            let buttonContainer = document.getElementById("buttonContainer");
+            buttonContainer.innerHTML = '';
 
             let newButton = document.createElement("button");
             newButton.classList.add("newButton");
             newButton.textContent = "Play Again";
             newButton.type = "submit";
             newButton.addEventListener("click", () => {
+                win=false;
                 location.reload();
             });
 
-            let buttonContainer = document.getElementById("buttonContainer");
             buttonContainer.appendChild(newButton);
         }
     }
 });
+
+
+
+
+
+
