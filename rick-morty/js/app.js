fb8f52ae1e06396c95d642913259619a7df91909
Matin <mx2000sy@gmail.com>
2024-08-28 15:34:08 +0430

    update rick and morty

--- rick-morty/js/app.js
+++ rick-morty/js/app.js
@@ -1,39 +1,53 @@
 async function fetchCharacters() {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const data = await response.json();
-    console.log(data.results)
-    showCharacters(data.results);
+    try {
+        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await response.json();
+        console.log(data.results);
+        showCharacters(data.results);
+        searchCharacters(data.results);
+    } catch (error) {
+        console.error('Error fetching characters:', error);
+    }
 }
 
-
-function showCharacters(character) {
+function showCharacters(characters) {
     const characterLists = document.getElementById("character-list");
-    const countCharacters = document.getElementById("character-count");
-    countCharacters.textContent = character.length;
+    characterLists.innerHTML = '';
+    // const countCharacters = document.getElementById("character-count");
+    // countCharacters.textContent = characters.length;
 
-    character.forEach((e)=> {
+    characters.forEach((character) => {
         const div = document.createElement("div");
         div.classList.add('character-item');
         div.innerHTML = `
-            <img src="${e.image}" alt="${e.name}" width="50">
-            <div>
-                <strong>${e.name}</strong><br>
-                <span>${e.status}-${e.species}</span>
+            <img src="${character.image}" alt="${character.name}" width="50">
+            <div class="character-info">
+                <strong>${character.name}</strong><br>
+                <span>${character.status} - ${character.species}</span>
             </div>
-            <p>👁️</p>
+            <p>${character.status === 'Alive' ? '💚' : '🖤'}</p>
         `;
-        div.onclick = ()=> showDetailCharacters(e)
+        div.onclick = () => showDetailCharacters(character);
         characterLists.appendChild(div);
-    })
+    });
 }
 
 function showDetailCharacters(character) {
     document.getElementById("character-image").src = character.image;
-    document.getElementById("character-status").textContent = `${character.status} ${character.species}`;
-    document.getElementById("character-location").textContent = character.location.name;
-    document.getElementById("favourite-note").textContent = 'this character is already in your favourites';
-
+    document.getElementById("character-status").textContent = `${character.status} - ${character.species}`;
+    document.getElementById("character-location").textContent = `Last known location: ${character.location.name}`;
+    document.getElementById("favourite-note").textContent = 'This character already is in your favourites';
 }
-    
 
-fetchCharacters()
+function searchCharacters(characters) {
+    const searchInput = document.getElementById('search-input');
+    searchInput.addEventListener('input', ()=>{
+        const query = searchInput.value.toLowerCase();
+        const filter = characters.filter((character)=> 
+            character.name.toLowerCase().includes(query)
+        )
+        showCharacters(filter);
+    })
+}
+
+fetchCharacters();
